a03860d183ed147984825d686cfaff64cae6daf8
Muinul-Islam <muinul1022@gmail.com>
2023-11-14 20:43:17 +0600

    carts added to the server

--- index.js
+++ index.js
@@ -29,12 +29,26 @@ async function run() {
     await client.connect();
 
     const menuCollection = client.db('bistroDB').collection('menu');
+    const reviewsCollection = client.db('bistroDB').collection('reviews');
+    const cartCollection = client.db('bistroDB').collection('carts');
+
 
     app.get('/menu', async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
     })
 
+    app.get('/reviews', async (req, res) => {
+      const result = await reviewsCollection.find().toArray();
+      res.send(result);
+    })
+
+    // carts collection
+    app.post('/carts', async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
+    })
 
 
     await client.db("admin").command({ ping: 1 });
@@ -49,7 +63,7 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-  res.send('Welcome TO Restaurant');
+  res.send('Welcome To Restaurant');
 })
 
 app.listen(port, () => {
